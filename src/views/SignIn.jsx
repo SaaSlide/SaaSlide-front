22c4bed1fbdc609cea9874a1caff769ed650b2ac
PikkMoune71 <antoinepicard385@mail.com>
2022-02-11 16:27:42 +0100

    Composant Button & Input

--- src/views/SignIn.jsx
+++ src/views/SignIn.jsx
@@ -1,3 +1,13 @@
+import Button from '../components/button'
+import Input from '../components/input'
+
 export const SignIn = () => {
-  return <h2>SignIn</h2>
+  return (
+    <div>
+      <h2>Se Connecter</h2>
+      <Input type="email" password={false} placeholder="Email" />
+      <Input type="password" password={true} placeholder="Mot de passe" />
+      <Button type="submit" className="btn-secondary" title="Connexion" />
+    </div>
+  )
 }
