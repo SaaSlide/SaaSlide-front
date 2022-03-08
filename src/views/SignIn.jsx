00df88c5bdb8e9829ce9c63b027d13d7287d34a7
PikkMoune71 <antoinepicard385@mail.com>
2022-03-08 22:00:38 +0100

    signup and signin update

--- src/views/SignIn.jsx
+++ src/views/SignIn.jsx
@@ -3,11 +3,19 @@ import Input from '../components/input'
 
 export const SignIn = () => {
   return (
-    <div>
-      <h2>Se Connecter</h2>
-      <Input type="email" password={false} placeholder="Email" />
-      <Input type="password" password={true} placeholder="Mot de passe" />
-      <Button type="submit" className="btn-secondary" title="Connexion" />
+    <div className="container_auth">
+      <div className="auth-content">
+        <h2>Se Connecter</h2>
+        <div className="input-content">
+          <Input type="email" password={false} placeholder="Email" />
+          <Input type="password" password={true} placeholder="Mot de passe" />
+        </div>
+        <Button type="submit" className="btn-secondary" title="Connexion" />
+        <div className="already-registered">
+          <p>Pas encore inscrit ?</p>
+          <a>Créer un compte</a>
+        </div>
+      </div>
     </div>
   )
 }
--- src/views/SignUp.jsx
+++ src/views/SignUp.jsx
@@ -1,3 +1,26 @@
+import Button from '../components/button'
+import Input from '../components/input'
+import '../styles/authentification/authentification.scss'
+
 export const SignUp = () => {
-  return <h2>SignUp</h2>
+  return (
+    <div className="container_auth">
+      <div className="auth-content">
+        <h2>Créer un compte</h2>
+        <Input type="text" password={false} placeholder="Nom d'utilisateur" />
+        <Input type="email" password={false} placeholder="Email" />
+        <Input type="password" password={true} placeholder="Mot de passe" />
+        <Input
+          type="password"
+          password={true}
+          placeholder="Vérification mot de passe"
+        />
+        <Button type="submit" className="btn-secondary" title="S'inscrire" />
+        <div className="already-registered">
+          <p>Déjà inscrit ?</p>
+          <a>Connectez-vous</a>
+        </div>
+      </div>
+    </div>
+  )
 }
