86261e9ce672055a0153bf7f3cc5c3f2e753ff6e
charlescosneau <charles@maplab.green>
2022-06-01 23:43:07 +0200

    registration and connection pages linked to api

--- src/views/SignIn.jsx
+++ src/views/SignIn.jsx
@@ -1,14 +1,40 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import Button from '../components/button'
 import Input from '../components/input'
 import Logo from '../components/logo'
+import axios from 'axios'
 
 export const SignIn = () => {
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios
+      .post('/auth/login', {
+        mail: e.target[0].value,
+        password: e.target[1].value,
+      })
+      .then((res) => {
+        console.log(res)
+        const date = new Date(Date.now() + 86400000)
+        document.cookie = `tokensaaslide=${
+          res.data.token
+        }; expires=${date.toUTCString()}; sameSite=strict`
+        const TokenContext = React.createContext(res.data.token)
+        navigate('/')
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
   return (
     <div className="container_auth">
       <Logo width="180" />
       <div className="auth-content login">
         <h2>Se Connecter</h2>
-        <form className="login-form">
+        <form onSubmit={handleSubmit} className="login-form">
           <div className="input-content">
             <Input type="email" password={false} placeholder="Email" />
             <Input type="password" password={true} placeholder="Mot de passe" />
