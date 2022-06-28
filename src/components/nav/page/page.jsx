6c8df14c0620a73fc46190c7a47133ca89d36bf2
BastianPEIRE <bastian@triliz.com>
2022-06-28 14:55:59 +0200

    feat(sidebar): possibilite de cacher la sidebar

--- src/components/nav/page/page.jsx
+++ src/components/nav/page/page.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './page.scss'
 import { Content } from '../content/content'
 import { SideBar } from '../sidebar/sidebar'
 
+export const ToggleSideBarContext = React.createContext()
+
 export const Page = () => {
+  const [ToggleSidebar, setToggleSidebar] = useState(true)
   return (
+    <ToggleSideBarContext.Provider value={[ToggleSidebar, setToggleSidebar]}>
     <div className="page">
       <SideBar />
       <Content />
     </div>
+    </ToggleSideBarContext.Provider>
   )
 }
--- src/components/nav/sidebar/sidebar.jsx
+++ src/components/nav/sidebar/sidebar.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './sidebar.scss'
 import { Profile } from '../profile/profile'
 import Logo from '../../logo/index'
+import { ToggleSideBarContext } from '../page/page'
 
 export const SideBar = () => {
+  const [sideBarContext, setSidebarContext] = useContext(ToggleSideBarContext)
+
   return (
+    <div>
+      {sideBarContext && (
     <div className="sidebar">
       <Logo width="100px" />
       <div>NavLinks</div>
       <Profile />
+        </div>
+      )}
     </div>
   )
 }
