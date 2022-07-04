9d3bf113981c50ba1ecf5560953c5effe47d8332
Mathieu <66616432+PMathieu16@users.noreply.github.com>
2022-07-04 16:45:11 +0200

    Test socket

--- src/views/mobile/viewerInterface/viewerInterface.jsx
+++ src/views/mobile/viewerInterface/viewerInterface.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import './viewerInterface.scss'
 import SocketProvider from '../../../utils/socket'
+import Test from './test'
 
 export const ViewerInterface = () => {
   const params = useParams()
+
   return (
     <SocketProvider room={params.diapoId} pseudo="toto">
       <h1>coucou</h1>
+      <Test></Test>
     </SocketProvider>
   )
 }
