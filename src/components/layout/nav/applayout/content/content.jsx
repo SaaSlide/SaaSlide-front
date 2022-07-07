017634a3079bc202f50c27c3769715d5b7f9dd71
PikkMoune71 <antoinepicard385@mail.com>
2022-07-07 13:30:30 +0200

    fix border-radius on pdflist and modif css links

--- src/components/layout/nav/applayout/content/content.jsx
+++ src/components/layout/nav/applayout/content/content.jsx
@@ -1,8 +1,10 @@
+import { useIsMobile } from '../../../../../utils/hooks'
 import './content.scss'
 
 export const Content = ({ components, category }) => {
+  const isMobile = useIsMobile()
   return (
-    <div className="content">
+    <div className={!isMobile && 'content'}>
       <div>{category}</div>
       <div>{components}</div>
     </div>
