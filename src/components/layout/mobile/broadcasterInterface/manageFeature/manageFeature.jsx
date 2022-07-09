445f22411102287824b6438400b2ddd11e6688a5
BastianPEIRE <bastian@triliz.com>
2022-07-09 14:07:27 +0200

    fix topNav adding functions

--- src/components/layout/mobile/broadcasterInterface/manageFeature/manageFeature.jsx
+++ src/components/layout/mobile/broadcasterInterface/manageFeature/manageFeature.jsx
@@ -2,7 +2,7 @@ import './manageFeature.scss'
 import { FeatureModal } from './featureModal/featureModal'
 import { useState } from 'react'
 
-export const ManageFeature = ({ type }) => {
+export const ManageFeature = ({ type, features }) => {
   const [openModal, setOpenModal] = useState(false)
 
   return (
@@ -16,7 +16,10 @@ export const ManageFeature = ({ type }) => {
         <h1>{type.title}</h1>
         <div className="feature-icon-broadcast">{type.icon}</div>
       </div>
-      {openModal && <FeatureModal />}
+      {openModal && <FeatureModal closeModal={setOpenModal} infos={features} />}
+      {/* {features.map((info) => (
+        <FeatureModal key={info} infos={info} />
+      ))} */}
     </div>
   )
 }
--- src/components/layout/mobile/broadcasterInterface/topnav/topNav.jsx
+++ src/components/layout/mobile/broadcasterInterface/topnav/topNav.jsx
@@ -1,10 +1,18 @@
+import { useNavigate } from 'react-router-dom'
+import { DeleteCookie } from '../../../../../services/apiUser'
 import './topnav.scss'
 
 export const TopNav = ({ specCount }) => {
+  const navigation = useNavigate()
+  const Logout = () => {
+    DeleteCookie()
+    navigation('/mobile/broadcastLogin')
+  }
+
   return (
     <div className="topnav-broadcaster">
       <div className="topbar-broadcaster">
-        <button>
+        <button onClick={() => Logout()}>
           <img src="/assets/icons/disconnect.svg" alt="" />
         </button>
         <div className="number-spec">
