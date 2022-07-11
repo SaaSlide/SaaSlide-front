56d04f5cb0a4be8fdcc290bd0b62df3121b05ae7
unknown <mathieu.parinet@gmail.com>
2022-07-12 00:39:54 +0200

    update

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,16 +4,21 @@ import {
   SondageHome,
   PresentationHome,
 } from '../components/layout/home'
+import FloatSmiley from '../components/layout/home/floatSmiley/floatSmiley'
 import { Footer } from '../components/layout/general'
+import SocketProvider from '../utils/socket'
 
 export const Home = () => {
   return (
     <>
-      <HeaderHome />
-      <SolutionHome />
-      <SondageHome />
-      <PresentationHome />
-      <Footer />
+      <SocketProvider room={'1'} name="test">
+        <FloatSmiley />
+        {/* <HeaderHome />
+        <SolutionHome />
+        <SondageHome />
+        <PresentationHome />
+        <Footer /> */}
+      </SocketProvider>
     </>
   )
 }
