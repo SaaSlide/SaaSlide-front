564c879903b43452be70aa3e7643d5ad416841a4
unknown <mathieu.parinet@gmail.com>
2022-07-14 01:09:44 +0200

    fix blink + resize canvas

--- src/components/business/diapoPresentation/floatSmiley/floatSmiley.jsx
+++ src/components/business/diapoPresentation/floatSmiley/floatSmiley.jsx
@@ -5,7 +5,7 @@ import './floatSmiley.scss'
 
 export const FloatSmiley = () => {
   const { socket } = useContext(SocketContext)
-
+  const ref = useRef()
   let canvas, ctx, w, h
 
   let emojis = []
@@ -27,48 +27,63 @@ export const FloatSmiley = () => {
   function draw() {
     ctx.clearRect(0, 0, w, h)
     emojis.forEach((p, i) => {
-      if (p.y < 100) {
-        p.opacity -= 0.01
-        p.y -= 0.5
-      }
+      if (p.y > -300) {
+        if (p.y < 100) {
+          p.opacity -= 0.01
+          p.y -= 0.5
+        }
 
-      if (p.y < h / 3) {
-        p.x += 0.01
-      } else if (p.y < h / Math.round(randomize(2, 1.5))) {
-        p.x += randomize(0, 0.5)
-      } else {
-        p.x -= 0.1
-      }
-      if (p.y <= -300) {
-        emojis.splice(i, 1)
-      }
+        if (p.y < h / 3) {
+          p.x += 0.01
+        } else if (p.y < h / Math.round(randomize(2, 1.5))) {
+          p.x += randomize(0, 0.5)
+        } else {
+          p.x -= 0.1
+        }
 
-      p.y -= p.speed * 10
-
-      ctx.save()
-      ctx.globalAlpha = p.opacity
-      ctx.setTransform(p.size, 0, 0, p.size, p.x, p.y)
-      ctx.fillText(p.smiley, 0, 40, 40, 0, 0, 40, 40)
-      ctx.restore()
+        p.y -= p.speed * 10
+        ctx.save()
+        ctx.globalAlpha = p.opacity
+        ctx.setTransform(p.size, 0, 0, p.size, p.x, p.y)
+        ctx.fillText(p.smiley, 0, 40, 40, 0, 0, 40, 40)
+        ctx.restore()
+      }
     })
 
     requestAnimationFrame(draw)
   }
 
-  useEffect(() => {
-    canvas = document.getElementById('canvas-smiley')
+  const setCanvas = () => {
+    canvas = ref.current
     ctx = canvas.getContext('2d')
-    w = canvas.clientWidth
-    h = canvas.clientHeight
+    w = window.innerWidth
+    h = window.innerHeight
     canvas.width = w
     canvas.height = h
+  }
+
+  document.addEventListener('fullscreenchange', () => {
+    setCanvas()
+  })
+
+  useEffect(() => {
+    setCanvas()
 
     draw()
 
     socket.on('get_smiley', (smiley) => {
       emojis.push(new randomReaction(smiley))
     })
+  }, [])
+
+  document.addEventListener('fullscreenchange', () => {
+    w = window.innerWidth
+    h = window.innerHeight
+    canvas.width = w
+    canvas.height = h
   })
 
-  return <canvas id="canvas-smiley" className="canvas-smiley"></canvas>
+  return (
+    <canvas ref={ref} id="canvas-smiley" className="canvas-smiley"></canvas>
+  )
 }
